43ee75ba83b03283311492d7d73a3765f00c0887
TreyDettmer <dettmertrey@gmail.com>
2023-07-01 10:31:59 -0700

    edited webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
+
+
 module.exports = {
+  env: {
+    MAPBOX_API_ACCESS_TOKEN: process.env.MAPBOX_API_ACCESS_TOKEN,
+    SITE_ACCESS_SHA256: process.env.SITE_ACCESS_SHA256
+  },
   resolve: {
     fallbacks:{
       "path": require.resolve("path-browserify"),
