6e7f7f432cb180f81a47b7668befe23b4deebed7
TreyDettmer <dettmertrey@gmail.com>
2023-07-01 10:36:03 -0700

    modified webpack.config.js again

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,11 @@ module.exports = {
       "os": require.resolve("os-browserify/browser"),
       "crypto": require.resolve("crypto-browserify")
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.MAPBOX_API_ACCESS_TOKEN': JSON.stringify(process.env.MAPBOX_API_ACCESS_TOKEN),
+      'process.env.SITE_ACCESS_SHA256': JSON.stringify(process.env.SITE_ACCESS_SHA256),
+    })
+  ]
 }
