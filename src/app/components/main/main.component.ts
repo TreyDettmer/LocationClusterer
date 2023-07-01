baa6dc69b98c2151a01e046c9aa01270003fc74e
TreyDettmer <dettmertrey@gmail.com>
2023-07-01 13:45:50 -0700

    removed unnecessary comments

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -847,7 +847,7 @@ export class MainComponent implements AfterViewInit{
     }
     let locationsArrayString = locationsArrayString1.join(';');
 
-    let accessToken : any = environment.MAPBOX_API_ACCESS_TOKEN; //"pk.eyJ1IjoidHJleWRldHRtZXIiLCJhIjoiY2xoOGRsd3JzMDQ1ejNkbzZiZmNvNGNxaCJ9.fGOdn3Ju30RKt11nZ_fBww"; //
+    let accessToken : any = environment.MAPBOX_API_ACCESS_TOKEN;
     let baseURL = "https://valhalla1.openstreetmap.de/optimized_route/";
     let url = `https://api.mapbox.com/optimized-trips/v1/mapbox/driving/${locationsArrayString}?steps=true&access_token=${accessToken}`;
     return this.http.get<any>(url).pipe(
